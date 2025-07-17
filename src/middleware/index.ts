1524d5c898b2cfcf20b0ce0871ff1d80408c035c
Gopal Sasmal <gopalsasmal100xdevs@gmail.com>
2025-07-17 15:02:00 +0530

    :triangular_flag_on_post: Remove rate limit package & added own rate limit

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,7 +1,6 @@
 import {
-  loginRateLimiter,
   loginRateLimiterMiddleware,
 } from "./rateLimiter.middleware";
 import { isAuthenticated } from "./auth.middleware";
 
-export { isAuthenticated, loginRateLimiter, loginRateLimiterMiddleware };
+export { isAuthenticated, loginRateLimiterMiddleware };
